be2325af05f4a0f50a0e150f05b2fcc2c87a2537
valb-mig <valb.mig@gmail.com>
2023-04-09 13:57:11 -0300

    [Update Scss + Create task done]

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -4,24 +4,23 @@ import './scss/Task.scss';
 
 const Task = (props) => {
     return(
-        <div className='task'>
-
+        <div id={props.id} className={'task '+props.class}>
             <div className='task-header'>
-                <div className='remove-button' onClick={props.remove}>
-                    <i className='fa fa-close'></i>
-                </div>
-            </div>
-
-            <div className='task-bottom'>
-                <div className='task-content'>
-                    {props.title}
-        
-                    <div className='task-desc'>
-                        {props.desc}
+                <div className='task-bottom'>
+                    <div className='task-content'>
+                        {props.title}
                     </div>
                 </div>
+                <div className='done-button' onClick={props.done}>
+                    <i className='fa fa-check'></i>
+                </div>
+                <div className='remove-button' onClick={props.remove}>
+                    <i className='fa fa-trash'></i>
+                </div>
             </div>
-
+            <div className='task-desc'>
+                {props.desc}
+            </div>
         </div>
     );
 }
