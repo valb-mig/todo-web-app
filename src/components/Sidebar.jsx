c13b23e05e9905b3f7096951ef667f6d11a35f79
valb-mig <valb.mig@gmail.com>
2023-04-16 18:02:46 -0300

    [Add Projects]

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Button from './Button';
+import Modal  from './Modal';
 
 import './scss/Sidebar.scss';
 
 const Sidebar = (props) => {
+
+    const [projects,   setProjects]   = useState([]);   
+    const [title,      setTitleInput] = useState('');
+    const [modal,      showModal]     = useState(false);
+    const [modalInput, setModalInput] = useState('');
+
+    let home = 'selected';
+
+    const handleProjectAdd = () => {
+
+        let selected = 'selected';
+
+        let project_title;
+        let project_select;
+
+        if(projects.length === 0)
+        {
+            project_title = 'Todo';
+            project_select = selected;
+
+            setProjects ([...projects,
+                {
+                    title:  project_title,
+                    select: project_select
+                }
+            ]);
+        }
+        else
+        {
+            showModalProject();
+        }
+    }
+    
+    const showModalProject = () => {
+        showModal(true);
+    }
+    
+    const getModalInput = (event) => {
+        setModalInput(event.target.value);
+    }
+
+    const submitProject = () => {
+    
+        setProjects([...projects, 
+            {
+                title:   modalInput,
+                select: 'selected'
+            }
+        ]);
+    
+        showModal(false);
+    }
+
     return(
         <div className='sidebar bg-gray-alt'>
 
@@ -13,31 +67,35 @@ const Sidebar = (props) => {
                     <Button
                         title='Home'
                         icon='home'
-                        // onclick={''}
-                    />
-                    <Button
-                        title=''
-                        icon=''
-                        // onclick={''}
-                    />
-                    <Button
-                        title=''
-                        icon=''
-                        // onclick={''}
+                        class={'todo-sidebar '+home}
                     />
                     <div className='comming-soon'>Comming soon</div>
                 </div>
                 <div className='sidebar-projects'>
                     Projects
-                    <Button
-                        title='Todo'
-                        icon='list'
-                        // onclick={''}
-                    />
+
+                    {projects.map((project, index)=>(
+                        <Button
+                            id={index}
+                            key={index}
+                            title={project.title}
+                            icon='list'
+                            class={'todo-sidebar '+project.select}
+                        />
+                    ))}
+
+                    {modal && (
+                        <Modal
+                            onchange={getModalInput}
+                            addClick={submitProject}
+                            cancelClick={()=>{showModal(false)}}
+                        />
+                    )}
+                    
                     <div className='add-button'>
                         <Button
                             icon='plus'
-                            // onclick={''}
+                            onclick={handleProjectAdd}
                         />
                     </div>
                 </div>
