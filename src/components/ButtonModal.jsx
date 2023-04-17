3d9fad7f8725e7debe21eb8fd7574eaa02caa449
Mig <mig@pop-os.localdomain>
2023-04-17 05:57:35 -0300

    [Update Scss]

---
+++ src/components/ButtonModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './scss/ButtonModal.scss'
+
+const ButtonModal = (props) => {
+    return(
+        <div className={props.class} onClick={props.onclick}>
+            {props.title}
+        </div>
+    );
+}
+export default ButtonModal;
