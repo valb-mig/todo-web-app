257abde1cc83703011d481a87778f6f0639a0246
valb-mig <valb.mig@gmail.com>
2023-06-29 00:20:25 -0300

    [Integrate laravel]

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,12 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\userController;
 
-// Reminder
-// rota -> controller -> action
-
 Route::post('/login',    [userController::class, 'login']); 
 Route::post('/register', [userController::class, 'register']); 
-
 
 Route::get('/', function(){
     return response()->json([
