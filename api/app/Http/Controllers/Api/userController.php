c245ad6fb079db5f4e3cf9a631790540d848f97d
valb-mig <mig@pop-os.localdomain>
2023-06-29 00:14:17 -0300

    [Integrate laravel]

--- api/app/Http/Controllers/Api/userController.php
+++ api/app/Http/Controllers/Api/userController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Http\Restaurant;
 
 class userController extends Controller 
 {
