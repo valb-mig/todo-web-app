b99a08d61d24dc4911826e76e5ae36f47eada0c4
valb-mig <valb.mig@gmail.com>
2023-10-01 04:11:48 -0300

    :bug:(fix): Deploy vercel ajsut

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,126 +1,23 @@
-"use client";
+'use client';
 
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
-
-import { 
-    BiSolidUser,
-    BiSolidLockAlt 
-} from 'react-icons/bi';
-
-import Link from 'next/link';
-
-import Header from '@/app/components/Header'; 
-import Input  from '@/app/components/Input';
-import Button from '@/app/components/Button'; 
-
-import handleLogin from '@/utils/api/user/login';
-
-import '@/app/login/styles/page.scss';
-
-export default function Login() {
-
-    const router = useRouter();
-
-    const [loginFormData, setLoginFormData] = useState({
-        'username':{
-            'value':'',
-            'error':false
-        },
-        'password':{
-            'value':'',
-            'error':false
-        }
-    })
-
-    const cleanFormData = () => {
-        setLoginFormData({
-            'username':{
-                'value':'',
-                'error':false
-            },
-            'password':{
-                'value':'',
-                'error':false
-            }
-        })
-    }
-
-    async function postFormData(event){
-        
-        event.preventDefault();
-
-        let response = await handleLogin(loginFormData);
-
-        if (!response) {
-
-            setLoginFormData({
-                ...loginFormData,
-                username: {
-                    ...loginFormData.username,
-                    error: true
-                },
-                password: {
-                    ...loginFormData.password,
-                    error: true
-                }
-            })
-        }
-        else {
-            cleanFormData();
-            router.push("/");
-        }
-    }
+export default function Login(){
 
     return (
-        <section className='login-page'>
+      <section className='login-page'>
 
-            <header className='header-box'>
-                <Header/>
-            </header>
+            <header className='header-box'/>
 
             <main className='main-box'>
                 <div className='content'>
                     <section className='box-container'>
-                        <form onSubmit={(e) => postFormData(e)}>
-                            <Input
-                                Type="text"
-                                Error={loginFormData.username.error}
-                                OnChange={(e) => setLoginFormData({
-                                    ...loginFormData,
-                                    username: {
-                                        ...loginFormData.username,
-                                        value: e.target.value,
-                                        error:false
-                                    }
-                                })}
-                                Icon={<BiSolidUser/>}
-                            />
-                            <Input
-                                Type="password"
-                                Error={loginFormData.username.error}
-                                OnChange={(e) => setLoginFormData({
-                                    ...loginFormData,
-                                    password: {
-                                        ...loginFormData.password,
-                                        value: e.target.value,
-                                        error:false
-                                    }
-                                })}
-                                Icon={<BiSolidLockAlt/>}
-                            />
-                            <div className='buttons'>
-                                <Button
-                                    Type="submit"
-                                    Title="Login"
-                                />
-                            </div>
-                            <p>Don't have an account yet ? <Link href={'/register'}><b><u> Register </u></b></Link></p>
+                        <form>
+                            <div className='buttons'/>
+                            <p/>
                         </form>
                     </section>
                 </div>
             </main>
-
+            
         </section>
-    )
+    );
 }
--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,126 +1,23 @@
-"use client";
+'use client'
 
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
-
-import { 
-    BiSolidUser,
-    BiSolidLockAlt 
-} from 'react-icons/bi';
-
-import Link from 'next/link';
-
-import Header from '@/app/components/Header'; 
-import Input  from '@/app/components/Input';
-import Button from '@/app/components/Button'; 
-
-import '@/app/register/styles/page.scss';
-
-export default function Register() {
-
-    const router = useRouter();
-
-    const [registerFormData, setRegisterFormData] = useState({
-        'username':{
-            'value':'',
-            'error':false
-        },
-        'password':{
-            'value':'',
-            'error':false
-        },
-        'confirm_password':{
-            'value':'',
-            'error':false
-        }
-    })
-
-    const cleanFormData = () => {
-        setRegisterFormData({
-            'username':{
-                'value':'',
-                'error':false
-            },
-            'password':{
-                'value':'',
-                'error':false
-            },
-            'confirm_password':{
-                'value':'',
-                'error':false
-            }
-        })
-    }
-
-    async function postFormData(event){
-        
-        event.preventDefault();
-
-        /* [TODO] Insert user function */
-    }
+export default function Register(){
 
     return (
-        <section className='login-page'>
+      <section className='login-page'>
 
-            <header className='header-box'>
-                <Header/>
-            </header>
+            <header className='header-box'/>
 
             <main className='main-box'>
                 <div className='content'>
                     <section className='box-container'>
-                        <form onSubmit={(e) => postFormData(e)}>
-                            <Input
-                                Type="text"
-                                Error={registerFormData.username.error}
-                                OnChange={(e) => setRegisterFormData({
-                                    ...registerFormData,
-                                    username: {
-                                        ...registerFormData.username,
-                                        value: e.target.value,
-                                        error:false
-                                    }
-                                })}
-                                Icon={<BiSolidUser/>}
-                            />
-                            <Input
-                                Type="password"
-                                Error={registerFormData.username.error}
-                                OnChange={(e) => setRegisterFormData({
-                                    ...registerFormData,
-                                    password: {
-                                        ...registerFormData.password,
-                                        value: e.target.value,
-                                        error:false
-                                    }
-                                })}
-                                Icon={<BiSolidLockAlt/>}
-                            />
-                            <Input
-                                Type="password"
-                                Error={registerFormData.username.error}
-                                OnChange={(e) => setRegisterFormData({
-                                    ...registerFormData,
-                                    confirm_password: {
-                                        ...registerFormData.confirm_password,
-                                        value: e.target.value,
-                                        error:false
-                                    }
-                                })}
-                                Icon={<BiSolidLockAlt/>}
-                            />
-                            <div className='buttons'>
-                                <Button
-                                    Type="submit"
-                                    Title="Register"
-                                />
-                            </div>
-                            <p>You have a account <Link href={'/login'}><b><u> Login </u></b></Link></p>
+                        <form>
+                            <div className='buttons'/>
+                            <p/>
                         </form>
                     </section>
                 </div>
             </main>
-
+            
         </section>
-    )
+    );
 }
